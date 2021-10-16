7f6d5639cf25f7e0b7ccef43f2625947f14a3f9e
BetaCat <60107867+timmywanttolearn@users.noreply.github.com>
2021-10-16 18:35:26 +0800

    no need

--- model_test/worker1.py
+++
@@ -1,6 +0,0 @@
-import torch.distributed.rpc as rpc
-import os
-os.environ['MASTER_ADDR'] = 'localhost'
-os.environ['MASTER_PORT'] = '5678'
-rpc.init_rpc("worker1", rank=1, world_size=2)
-rpc.shutdown()
